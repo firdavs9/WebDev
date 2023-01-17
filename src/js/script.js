f6185e397fbae74b0c1b4f433959d96636d73ce2
Firdavs <rakhmoov.firdavs9@gmail.com>
2023-01-17 17:09:36 +0300

    animate catalog item and feedback

--- src/js/script.js
+++ src/js/script.js
@@ -115,6 +115,8 @@ $(document).ready(function(){
       const _href = $(this).attr("href");
       $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
       return false;
-    })
+    });
+
+    new WOW().init();
 
 });
